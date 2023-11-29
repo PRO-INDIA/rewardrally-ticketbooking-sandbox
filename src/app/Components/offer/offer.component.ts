650d3ccaad0a1641f4137f0b24087cd7e0cfc62d
Gunasekaran19 <gunasekaran@theproindia.com>
2023-11-29 11:23:21 +0530

    fix: image issue

--- src/app/Components/offer/offer.component.ts
+++ src/app/Components/offer/offer.component.ts
@@ -7,8 +7,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class OfferComponent implements OnInit {
   @Input() offer: any;
-  sasToken: string =
-    '?sv=2020-10-02&si=sandbox-assets-188BDA9327F&sr=c&sig=kHlt0prnMYgLIWMPASPmU2VN%2BlxXICXJMkEkV863RoE%3D';
   constructor() {}
 
   ngOnInit(): void {}
